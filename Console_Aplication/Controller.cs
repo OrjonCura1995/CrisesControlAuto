c1eea99e6060c60f61bbe87c59c4cda0d691bec5
Orjon.Cura <Orjon.Cura@TL004>
2017-10-24 12:22:36 +0100

    commit

--- Console_Aplication/Controller.cs
+++ Console_Aplication/Controller.cs
@@ -10,7 +10,7 @@ namespace Console_Aplication
             driver = new ChromeDriver();
             text = "";
 
-            //Feedback(Password.Reset, "Password Reset");
+            Feedback(Password.Reset, "Password Reset");
 
             Console.WriteLine("Please Enter Your Email");
             email = Console.ReadLine();
@@ -20,52 +20,52 @@ namespace Console_Aplication
 
             Initializer();
 
-            //Report.report();
+            Report.report();
 
             Feedback(Incident.Create, "Create Incident");
             Incident.Task();
             Feedback(Incident.Launch, "Launch Incident");
             Incident.Task2();
-            //Incident.IncidentDetails();
+            Incident.IncidentDetails();
             Feedback(Incident.Deactivate, "Deactivate Incident");
             Feedback(Incident.Edit, "Edit Incident");
             Feedback(Incident.Delete, "Delete Incident");
 
-            //Feedback(Location.Create, "Create Location");
-            //Feedback(Location.Edit, "Edit Location");
-            //Feedback(Location.Delete, "Delete Location");
+            Feedback(Location.Create, "Create Location");
+            Feedback(Location.Edit, "Edit Location");
+            Feedback(Location.Delete, "Delete Location");
 
-            //Feedback(Group.Create, "Create Group");
-            //Feedback(Group.Edit, "Edit Group");
-            //Feedback(Group.Delete, "Delete Group");
+            Feedback(Group.Create, "Create Group");
+            Feedback(Group.Edit, "Edit Group");
+            Feedback(Group.Delete, "Delete Group");
 
-            //Feedback(Department.Create, "Create Department");
-            //Feedback(Department.Edit, "Edit Department");
-            //Feedback(Department.Delete, "Delete Department");
+            Feedback(Department.Create, "Create Department");
+            Feedback(Department.Edit, "Edit Department");
+            Feedback(Department.Delete, "Delete Department");
 
-            //Feedback(Asset.Create, "Create Asset");
-            //Feedback(Asset.Edit, "Edit Asset");
-            //Feedback(Asset.Delete, "Delete Asset");
+            Feedback(Asset.Create, "Create Asset");
+            Feedback(Asset.Edit, "Edit Asset");
+            Feedback(Asset.Delete, "Delete Asset");
 
-            //Feedback(User.Create, "Create User");
-            //Feedback(User.Edit, "Edit User");
-            //Feedback(User.Delete, "Delete User");
+            Feedback(User.Create, "Create User");
+            Feedback(User.Edit, "Edit User");
+            Feedback(User.Delete, "Delete User");
 
-            //Feedback(ISOP_Wizard.Create, "Create ISOP Wizard");
-            //Feedback(ISOP_Wizard.Delete, "Delete ISOP Wizard");
+            Feedback(ISOP_Wizard.Create, "Create ISOP Wizard");
+            Feedback(ISOP_Wizard.Delete, "Delete ISOP Wizard");
 
-            //Feedback(Scheduler.Schedule, "Schedule Ping");
-            //Feedback(Scheduler.Edit, "Edit Scheduled Ping");
-            //Feedback(Scheduler.Delete, "Delete Scheduled Ping");
+            Feedback(Scheduler.Schedule, "Schedule Ping");
+            Feedback(Scheduler.Edit, "Edit Scheduled Ping");
+            Feedback(Scheduler.Delete, "Delete Scheduled Ping");
 
-            //Feedback(Trigger.Create, "Trigger Ping");
-            //Feedback(Trigger.Edit, "trigger Triggered Ping");
-            //Feedback(Trigger.Delete, "Delete Triggered Ping");
+            Feedback(Trigger.Create, "Trigger Ping");
+            Feedback(Trigger.Edit, "trigger Triggered Ping");
+            Feedback(Trigger.Delete, "Delete Triggered Ping");
 
-            //Feedback(Company_Setup.Modifiy, "Company Setup");
-            //Feedback(Company_Setup.Modifiy2, "Company Setup");
-            //Feedback(Ping.Create, "Create Ping");
-            //Feedback(Ping.Acknowledge, "Acknowledge Ping");
+            Feedback(Company_Setup.Modifiy, "Company Setup");
+            Feedback(Company_Setup.Modifiy2, "Company Setup");
+            Feedback(Ping.Create, "Create Ping");
+            Feedback(Ping.Acknowledge, "Acknowledge Ping");
 
             txt(text);
 
